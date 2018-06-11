ac2a55c16ab439153af1a87f85c9388283b9f6a2
GoForBroke <go.for.broke1006@gmail.com>
2018-06-11 15:49:02 +0300

    Scaffolding

--- src/main/java/com/raccoonberus/chatbot/MainServlet.java
+++ src/main/java/com/raccoonberus/chatbot/MainServlet.java
@@ -1,18 +1,16 @@
 package com.raccoonberus.chatbot;
 
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        resp.setContentType("text/plain; charset=UTF-8");
-        ServletOutputStream outputStream = resp.getOutputStream();
-        outputStream.print("Test message!!!");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter writer = resp.getWriter();
+        writer.print("Test message 2!!!");
     }
 }
