42bcb6919dd999aab0150faafa08cb5038790ec9
Arina <thearinamarr@gmail.com>
2018-06-16 01:24:38 +0700

    some start to parsing updates

--- src/main/java/com/raccoonberus/chatbot/Application.java
+++ src/main/java/com/raccoonberus/chatbot/Application.java
@@ -11,7 +11,7 @@ public class Application {
     public static void main(String[] args) {
 //        System.out.print(getMessage());
         TelegramClient t = new TelegramClient();
-        t.send("Hello Arina! I'm bot!");
+        t.send("");
     }
 
     private static String answer(String message) {
--- src/main/java/com/raccoonberus/chatbot/connector/telegram/TelegramClient.java
+++ src/main/java/com/raccoonberus/chatbot/connector/telegram/TelegramClient.java
@@ -1,5 +1,8 @@
 package com.raccoonberus.chatbot.connector.telegram;
 
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -12,7 +15,8 @@ public class TelegramClient {
         String proxyAddr = System.getenv("TELEGRAM_PROXY");
 
         try {
-            URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatID + "&text=" + message);
+//            URL url = new URL("https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + chatID + "&text=" + message);
+            URL url = new URL("https://api.telegram.org/bot" + token + "/getUpdates?chat_id=" + chatID);
 
             URL proxyUrl = new URL(proxyAddr);
             Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(), proxyUrl.getPort()));
@@ -37,4 +41,62 @@ public class TelegramClient {
 
         return null;
     }
+
+    public class Employee {
+        private int id;
+        private String firstName;
+
+        // standard getters and setters
+    }
+
+    Client client = ClientBuilder.newClient();
+
+    WebTarget webTarget = client.target("http://localhost:8082/spring-jersey");
+
+    WebTarget employeeWebTarget = webTarget.path("resources/employees");
+
+    Invocation.Builder invocationBuilder = employeeWebTarget.request(MediaType.APPLICATION.JSON);
+
+    Response response = invocationBuilder.get(Employee.class);
+
+    Response response = invocationBuilder.post(Entity.entity(employee, MediaType.APPLICATION_JSON));
+
+    public class RestClient {
+
+        private static final String REST_URI = "http://localhost:8082/spring-jersey/resources/employees";
+
+        private Client client = ClientBuilder.newClient();
+
+        public Employee getJsonEmployee(int id) {
+            return client
+                    .target(REST_URI)
+                    .path(String.valueOf(id))
+                    .request(MediaType.APPLICATION_JSON)
+                    .get(Employee.class);
+        }
+        //...
+    }
+
+    public Response createJsonEmployee(Employee emp) {
+        return client
+                .target(REST_URI)
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(emp, MediaType.APPLICATION_JSON));
+    }
+
+    public class JerseyClientLiveTest {
+
+        public static final int HTTP_CREATED = 201;
+        private RestClient client = new RestClient();
+
+        @Test
+        public void givenCorrectObject_whenCorrectJsonRequest_thenResponseCodeCreated() {
+            Employee emp = new Employee(6, "Johny");
+
+            Response response = client.createJsonEmployee(emp);
+
+            assertEquals(response.getStatus(), HTTP_CREATED);
+        }
+    }
 }
+
