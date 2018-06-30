40de73569806f5ef81901c75bf002f2ecffd471b
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-06-30 20:24:43 +0300

    Fix parsing

--- src/main/java/com/raccoonberus/chatbot/connector/telegram/TelegramClient.java
+++ src/main/java/com/raccoonberus/chatbot/connector/telegram/TelegramClient.java
@@ -3,6 +3,7 @@ package com.raccoonberus.chatbot.connector.telegram;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raccoonberus.chatbot.connector.telegram.model.GetUpdatesResponse;
+import com.raccoonberus.chatbot.connector.telegram.model.ResultItem;
 import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
@@ -66,102 +67,31 @@ public class TelegramClient {
         Client client = ClientBuilder.newClient(config);
 
         String baseUrl = "https://api.telegram.org/bot" + token + "/";
-        System.out.println(baseUrl);
+//        System.out.println(baseUrl);
 
-        String response = client.target(baseUrl)
+        GetUpdatesResponse response = client.target(baseUrl)
                 .path("getUpdates")
                 .queryParam("chat_id", chatID)
                 .request(MediaType.APPLICATION_JSON)
 //                .post(Entity.entity(null, MediaType.APPLICATION_JSON), GetUpdatesResponse.class);
-//                .get(GetUpdatesResponse.class);
-                .get(String.class);
+                .get(GetUpdatesResponse.class);
+//                .get(String.class);
 
         System.out.println(response);
 
-        response = "{\n" +
-                "  \"ok\": true,\n" +
-                "  \"result\": [\n" +
-                "    {\n" +
-                "      \"update_id\": 934773487,\n" +
-                "      \"message\": {\n" +
-                "        \"message_id\": 22,\n" +
-                "        \"from\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"is_bot\": false,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"language_code\": \"en-US\"\n" +
-                "        },\n" +
-                "        \"chat\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"type\": \"private\"\n" +
-                "        },\n" +
-                "        \"date\": 1530372162,\n" +
-                "        \"text\": \"ghbdtn\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"update_id\": 934773488,\n" +
-                "      \"message\": {\n" +
-                "        \"message_id\": 23,\n" +
-                "        \"from\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"is_bot\": false,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"language_code\": \"en-US\"\n" +
-                "        },\n" +
-                "        \"chat\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"type\": \"private\"\n" +
-                "        },\n" +
-                "        \"date\": 1530372199,\n" +
-                "        \"text\": \"efweew\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"update_id\": 934773489,\n" +
-                "      \"message\": {\n" +
-                "        \"message_id\": 24,\n" +
-                "        \"from\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"is_bot\": false,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"language_code\": \"en-US\"\n" +
-                "        },\n" +
-                "        \"chat\": {\n" +
-                "          \"id\": 360663876,\n" +
-                "          \"first_name\": \"Sergey\",\n" +
-                "          \"last_name\": \"Cherkesov\",\n" +
-                "          \"username\": \"go_for_broke\",\n" +
-                "          \"type\": \"private\"\n" +
-                "        },\n" +
-                "        \"date\": 1530373822,\n" +
-                "        \"text\": \"dbdfbfd\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
+//        ObjectMapper mapper = new ObjectMapper();
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+//        GetUpdatesResponse r = mapper.readValue(response, GetUpdatesResponse.class);
+//        System.out.println(r);
 
-        ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-        GetUpdatesResponse r = mapper.readValue(response, GetUpdatesResponse.class);
-        System.out.println(r);
-
-//        System.out.println(response.isOk() ? "OK" : "Something wrong!");
-//        if (response.getResult().length > 0)
-//            System.out.println(response.getResult()[0].getUpdateId());
+        System.out.println(response.isOk() ? "OK" : "Something wrong!");
+        for (ResultItem item : response.getResult()) {
+            System.out.println(
+                    item.getMessage().getFrom().getUsername()
+                            + " -> "
+                            + item.getMessage().getText());
+            System.out.println("==========");
+        }
 
         return null;
     }
