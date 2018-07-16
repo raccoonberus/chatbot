1d1823e8f1cb539070f37563e60486bc61374654
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-07-16 03:36:28 +0300

    Start to setup travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+ssh root@${PRODUCTION_SERVER}
+
 cd ~/chatbot/
 git pull
-docker pull raccoonberus/chatbot:1.0.0
-bash bin/prod-run.sh
+
+IMAGE_NAME=chatbot_app
+DOCKER_ID_USER="raccoonberus"
+VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q 2> /dev/null)
+
+echo "Build: ${VERSION}"
+mvn clean package
+docker build -t $DOCKER_ID_USER/${IMAGE_NAME}:latest \
+    -t $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION} .
+docker push $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION}
+docker push $DOCKER_ID_USER/${IMAGE_NAME}:latest
+
+docker rm chatbot --force
+docker run -d --name chatbot --env-file ./.env -it raccoonberus/chatbot_app:1.0.0
