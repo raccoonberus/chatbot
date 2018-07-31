7cefc20f43374f9cf57a3cd56d900653d4a1021c
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-08-01 00:40:26 +0300

    Start to setup travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
+PID_FILE=/var/run/chatbot.pid
+
 sshpass -p $DEPLOY_PASSWORD ssh $DEPLOY_USER@$DEPLOY_HOST
 cd $DEPLOY_PATH
-#ssh -i .travis/github_deploy_key.enc -o "StrictHostKeyChecking no" root@${PRODUCTION_SERVER}
 
-#cd ~/chatbot/
-git pull
+git checkout master
+git pull origin master
 
-IMAGE_NAME=chatbot_app
-DOCKER_ID_USER="raccoonberus"
-VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q 2> /dev/null)
+if [[ -f ${PID_FILE} ]]; then
+    kill -9 "$(cat ${PID_FILE})"
+fi
 
-echo "Build: ${VERSION}"
-mvn clean package
-docker build -t $DOCKER_ID_USER/${IMAGE_NAME}:latest \
-    -t $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION} .
-docker push $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION}
-docker push $DOCKER_ID_USER/${IMAGE_NAME}:latest
+mvn clean package -DskipTests=true
+java -Dexternal.properties.file='chatbot.properties' -jar target/chatbot-1.0.0.jar &
+echo $! > ${PID_FILE}
 
-docker rm chatbot --force
-docker run -d --name chatbot --env-file ./.env -it raccoonberus/chatbot_app:1.0.0
+#IMAGE_NAME=chatbot_app
+#DOCKER_ID_USER="raccoonberus"
+#VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q 2> /dev/null)
+
+#echo "Build: ${VERSION}"
+#mvn clean package
+#docker build -t $DOCKER_ID_USER/${IMAGE_NAME}:latest \
+#    -t $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION} .
+#docker push $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION}
+#docker push $DOCKER_ID_USER/${IMAGE_NAME}:latest
+#
+#docker rm chatbot --force
+#docker run -d --name chatbot --env-file ./.env -it raccoonberus/chatbot_app:1.0.0
