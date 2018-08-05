a9adc4a386f69fb5de2e08a61a2f774b392c99e8
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-08-06 00:20:44 +0300

    Start to setup travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,14 +16,17 @@ echo 'Files updated'
 mvn clean package -DskipTests=true
 
 if [[ -f ${PID_FILE} ]]; then
-    kill $(ps aux | grep 'chatbot-' | awk '{print $2}')
+    kill $(ps aux | grep 'chatbot-' | awk '{print $2}') || true
 #    kill -9 "$(cat ${PID_FILE})" || true
 #    rm ${PID_FILE}
     echo 'Stop old version'
 fi
 
+
+echo 'Run new version'
 java -Dexternal.properties.file='chatbot.properties' -jar target/chatbot-1.0.0.jar &
 echo $! > ${PID_FILE}
+echo 'Success!!!'
 
 
 #IMAGE_NAME=chatbot_app
