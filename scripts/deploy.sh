9b2e7f5201a2015de014364aadedb98f930ac05c
GoForBroke1006 <go.for.broke1006@gmail.com>
2018-08-06 01:43:27 +0300

    Start to setup travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,13 +15,6 @@ echo 'Files updated'
 
 mvn clean package -DskipTests=true
 
-#if [[ -f ${PID_FILE} ]]; then
-#    kill $(ps aux | grep 'chatbot-' | awk '{print $2}') || true
-#    kill -9 "$(cat ${PID_FILE})" || true
-#    rm ${PID_FILE}
-#    echo 'Stop old version'
-#fi
-
 kill $(ps aux | grep 'chatbot-' | awk '{print $2}') || true
 
 echo 'Run new version'
@@ -29,18 +22,3 @@ export $(cat .env | xargs) && \
     java -Dexternal.properties.file='chatbot.properties' -jar target/chatbot-1.0.0.jar &> ./chatbot.log &
 #echo $! > ${PID_FILE}
 echo 'Success!!!'
-
-
-#IMAGE_NAME=chatbot_app
-#DOCKER_ID_USER="raccoonberus"
-#VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q 2> /dev/null)
-
-#echo "Build: ${VERSION}"
-#mvn clean package
-#docker build -t $DOCKER_ID_USER/${IMAGE_NAME}:latest \
-#    -t $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION} .
-#docker push $DOCKER_ID_USER/${IMAGE_NAME}:${VERSION}
-#docker push $DOCKER_ID_USER/${IMAGE_NAME}:latest
-#
-#docker rm chatbot --force
-#docker run -d --name chatbot --env-file ./.env -it raccoonberus/chatbot_app:1.0.0
