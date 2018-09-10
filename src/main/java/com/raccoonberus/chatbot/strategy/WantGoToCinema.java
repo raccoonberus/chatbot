1db822c9ab89438249373ed46eedd3f8a6338f23
Arina <thearinamarr@gmail.com>
2018-09-11 02:54:03 +0700

    movie's names are parsing

--- src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
+++ src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
@@ -1,33 +1,52 @@
 package com.raccoonberus.chatbot.strategy;
 
 import org.htmlparser.*;
+import org.htmlparser.filters.*;
 import org.htmlparser.util.NodeList;
 
 public class WantGoToCinema {
 
     private static final String keywords = "what about cinema?";
 
-    public String run() {
+    public String run(String address) {
 
-        String address = "https://www.google.com/search?ei=&q=что+в+кино&oq=что+в+кино";
+        String nameAttribute = "itemprop";
+        String nameAttributeValue = "name";
+//        String timeAttribute = "class";
+//        String timeAttributeValue = "buyTicketLead";
         try {
             Parser parser = new Parser(address);
-            NodeList nodeList = parser.parse(null);
+            HasAttributeFilter filter = new HasAttributeFilter (nameAttribute, nameAttributeValue);
+            NodeList nodeList = parser.parse(filter);
+            Node node;
 
             for (int i = 0; i < nodeList.size(); i++) {
 
-                Node node = nodeList.elementAt(i);
-                 System.out.println(node.toHtml());
+                node = nodeList.elementAt(i);
+                if (!node.getText().contains("кинотеатрах")) {
+                    String s = node.getText().replaceAll("&nbsp;", " ");
+                    int g = s.indexOf('"', 30);
+                    System.out.println(s.substring(30, g));
+                }
             }
 
+/*            filter = new HasAttributeFilter (timeAttribute, timeAttributeValue);
+            nodeList = parser.parse(filter);
+
+            for (int i = 0; i < nodeList.size(); i++) {
+
+                node = nodeList.elementAt(i);
+                String s = node.getText().replaceAll("&nbsp;", " ");
+                System.out.println(s);
+            }*/
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "something";
+        return "nothing";
     }
 
     public static void main (String args[]) {
 
-        System.out.println(new WantGoToCinema().run());
+        new WantGoToCinema().run("https://www.kinopoisk.ru/afisha/new/city/452/");
     }
 }
