4382a826730249dd1eae4fe598e2f9127f676882
Arina <thearinamarr@gmail.com>
2018-09-11 21:09:32 +0700

    almost grabbed schedule

--- src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
+++ src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
@@ -12,41 +12,68 @@ public class WantGoToCinema {
 
         String nameAttribute = "itemprop";
         String nameAttributeValue = "name";
-//        String timeAttribute = "class";
-//        String timeAttributeValue = "buyTicketLead";
+        String timeAttribute = "class";
+        String timeAttributeValue = "or";
+
         try {
             Parser parser = new Parser(address);
             HasAttributeFilter filter = new HasAttributeFilter (nameAttribute, nameAttributeValue);
             NodeList nodeList = parser.parse(filter);
-            Node node;
+
+            Parser timeParser = new Parser(address);
+            HasAttributeFilter timeFilter = new HasAttributeFilter (timeAttribute, timeAttributeValue);
+            NodeList timeNodeList = timeParser.parse(timeFilter);
+
+            for (int i = 0; i < timeNodeList.size(); i++) {
+
+                String s = timeNodeList.elementAt(i).getText();
+                s = "https://www.kinopoisk.ru/" + s.substring(20, s.length() - 1);
+                System.out.println(s);
+            }
 
             for (int i = 0; i < nodeList.size(); i++) {
-
-                node = nodeList.elementAt(i);
-                if (!node.getText().contains("кинотеатрах")) {
-                    String s = node.getText().replaceAll("&nbsp;", " ");
-                    int g = s.indexOf('"', 30);
-                    System.out.println(s.substring(30, g));
+                String s = nodeList.elementAt(i).getText().replaceAll("&nbsp;", " ");
+                if (!s.contains("кинотеатрах")) {
+                    s = s.substring(30, s.length() - 3);
+                    System.out.println(s);
                 }
             }
 
-/*            filter = new HasAttributeFilter (timeAttribute, timeAttributeValue);
-            nodeList = parser.parse(filter);
-
-            for (int i = 0; i < nodeList.size(); i++) {
-
-                node = nodeList.elementAt(i);
-                String s = node.getText().replaceAll("&nbsp;", " ");
-                System.out.println(s);
-            }*/
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "nothing";
     }
 
+    private String parseTime(String address) {
+
+        String attribute = "data-template";
+        String attributeValue = "schedule-item__template";
+
+        String s = null;
+        try {
+            Parser parser = new Parser(address);
+            HasAttributeFilter filter = new HasAttributeFilter (attribute, attributeValue);
+            NodeList nodeList = parser.parse(filter);
+
+            for (int i = 0; i < nodeList.size(); i++) {
+
+                s = nodeList.elementAt(i).getText();
+//                System.out.println(s.length());
+//                s = s.substring(200, s.length() - 7);
+                System.out.println(s);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+
     public static void main (String args[]) {
 
-        new WantGoToCinema().run("https://www.kinopoisk.ru/afisha/new/city/452/");
+//        new WantGoToCinema().run("https://www.kinopoisk.ru/afisha/new/city/452/");
+        new WantGoToCinema().parseTime("https://www.kinopoisk.ru/film/991097/afisha/city/452/");
     }
 }
