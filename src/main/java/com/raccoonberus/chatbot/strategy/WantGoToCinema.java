44e2aabe2d770972e2e49a30312d1702be0dc6e1
Arina <thearinamarr@gmail.com>
2018-09-14 22:03:02 +0700

    starting to parse mori-cinema

--- src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
+++ src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
@@ -10,37 +10,37 @@ public class WantGoToCinema {
 
     public String run(String address) {
 
-        String attributeMovieName = "itemprop";
-        String attributeMovieNameValue = "name";
-        String attributeSchedule = "class";
-        String attributeScheduleValue = "or";
+        String attribute = "data-day";
+        String attributeValue = "today";
+        String attributeMovieName = "target";
+//        String attributeFormat2DValue = "format format-2D";
+//        String attributeFormat3DValue = "format format-3D";
+//        String attributeTime = "kh:widget";
+//        String attributeTimeValue = "seance";
 
         try {
-            Parser nameParser = new Parser(address);
-            HasAttributeFilter nameFilter = new HasAttributeFilter (attributeMovieName, attributeMovieNameValue);
-            NodeList nodeList = nameParser.parse(nameFilter);
+            Parser parser = new Parser(address);
+            HasAttributeFilter filter = new HasAttributeFilter (attribute, attributeValue);
+            NodeList nodeList = parser.parse(filter);
+            address = nodeList.elementAt(1).toHtml();
 
-            Parser scheduleParser = new Parser(address);
-            HasAttributeFilter scheduleFilter = new HasAttributeFilter (attributeSchedule, attributeScheduleValue);
-            NodeList timeNodeList = scheduleParser.parse(scheduleFilter);
+            Parser nameParser = new Parser();
+            nameParser.setInputHTML(address);
+            HasAttributeFilter nameFilter = new HasAttributeFilter (attributeMovieName);
+            NodeList nameList = nameParser.parse(nameFilter);
+            String name;
 
-            String scheduleURL;
-            String movieName;
+//            Parser timeParser = new Parser();
+//            timeParser.setInputHTML(address);
+//            HasAttributeFilter timeFilter = new HasAttributeFilter (attributeTime, attributeTimeValue);
+//            NodeList timeList = timeParser.parse(timeFilter);
+//            String time;
 
-            for (int i = 0, j = 0; i < nodeList.size(); i++) {
-                movieName = nodeList.elementAt(i).getText().replaceAll("&nbsp;", " ");
+            for(int i = 0; i < nameList.size(); i++) {
 
-                if (!movieName.contains("кинотеатрах")) {
-
-                    movieName = movieName.substring(30, movieName.length() - 3);
-                    System.out.println(movieName + "\n");
-
-                    scheduleURL = timeNodeList.elementAt(j).getText();
-                    scheduleURL = "https://www.kinopoisk.ru/" + scheduleURL.substring(20, scheduleURL.length() - 1);
-//                    System.out.println(scheduleURL);
-                    parseSchedule(scheduleURL);
-                    j++;
-                }
+                name = nameList.elementAt(i).toHtml();
+                name = name.substring(name.indexOf('>') + 1, name.length() - 4);
+                System.out.println(name);
             }
 
         } catch (Exception e) {
@@ -49,54 +49,8 @@ public class WantGoToCinema {
         return "nothing";
     }
 
-    public String parseSchedule(String address) {
-
-        String attribute = "class";
-        String attributeValue = "schedule-item";
-        String attributePlaceValue = "schedule-cinema__name";
-        String attributeTime = "data-template";
-
-        try {
-            Parser parser = new Parser(address);
-            HasAttributeFilter filter = new HasAttributeFilter(attribute, attributeValue);
-            NodeList nodeList = parser.parse(filter);
-            String node;
-
-            Parser placeParser = new Parser();
-            HasAttributeFilter placeFilter = new HasAttributeFilter (attribute, attributePlaceValue);
-            String place;
-
-            Parser timeParser = new Parser();
-            HasAttributeFilter timeFilter = new HasAttributeFilter (attributeTime);
-            String time;
-
-            for(int i = 0; i < nodeList.size(); i++) {
-
-                node = nodeList.elementAt(i).toHtml();
-
-                timeParser.setInputHTML(node);
-                NodeList timeList = timeParser.parse(timeFilter);
-                time = timeList.elementAt(0).toHtml(); // it's not fine case it's random nearest (2D/3D/IMAX - whatever)
-                time = time.substring(time.length() - 12, time.length() - 7);
-                System.out.print(time + "  ");
-
-                placeParser.setInputHTML(node);
-                NodeList placeList = placeParser.parse(placeFilter);
-                place = placeList.elementAt(0).toHtml();
-                place = place.substring(101, place.length() - 4);
-                System.out.println(place);
-            }
-            System.out.println();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return "idk";
-    }
-
     public static void main (String args[]) {
 
-        new WantGoToCinema().run("https://www.kinopoisk.ru/afisha/new/city/452/");
-//        new WantGoToCinema().parseSchedule("");
+        new WantGoToCinema().run("https://mori-cinema.ru/cinema_detail/76_mori_sinema_krasnogorsk/schedule.php");
     }
 }
