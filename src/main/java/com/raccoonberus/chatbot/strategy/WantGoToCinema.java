d4495d71151d7af5b4ffa6be445f7384b4e51eef
Arina <thearinamarr@gmail.com>
2018-09-21 03:32:13 +0700

    mori cinema parsing

--- src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
+++ src/main/java/com/raccoonberus/chatbot/strategy/WantGoToCinema.java
@@ -1,56 +1,79 @@
 package com.raccoonberus.chatbot.strategy;
 
+import com.raccoonberus.chatbot.ReplyStrategy;
 import org.htmlparser.*;
 import org.htmlparser.filters.*;
 import org.htmlparser.util.NodeList;
 
-public class WantGoToCinema {
+public class WantGoToCinema implements ReplyStrategy {
 
-    private static final String keywords = "what about cinema?";
+    private static final String keywords = "what about cinema";
 
-    public String run(String address) {
+    @Override
+    public boolean isSupport(String message) {
+        return true;
+    }
 
-        String attribute = "data-day";
-        String attributeValue = "today";
-        String attributeMovieName = "target";
-//        String attributeFormat2DValue = "format format-2D";
-//        String attributeFormat3DValue = "format format-3D";
-//        String attributeTime = "kh:widget";
-//        String attributeTimeValue = "seance";
+    @Override
+    public boolean isSupport(String... keywords) {
+        return false;
+    }
+
+    @Override
+    public String run(String message) {
+        String address = "https://mori-cinema.ru/cinema_detail/76_mori_sinema_krasnogorsk/schedule.php";
 
         try {
             Parser parser = new Parser(address);
-            HasAttributeFilter filter = new HasAttributeFilter (attribute, attributeValue);
+            HasAttributeFilter filter = new HasAttributeFilter ("data-day", "today");
             NodeList nodeList = parser.parse(filter);
             address = nodeList.elementAt(1).toHtml();
 
+            Parser blockParser = new Parser();
+            blockParser.setInputHTML(address);
+            TagNameFilter blockFilter = new TagNameFilter("tr");
+            NodeList blockList = blockParser.parse(blockFilter);
+            String block;
+
             Parser nameParser = new Parser();
-            nameParser.setInputHTML(address);
-            HasAttributeFilter nameFilter = new HasAttributeFilter (attributeMovieName);
-            NodeList nameList = nameParser.parse(nameFilter);
+            HasAttributeFilter nameFilter = new HasAttributeFilter ("target");
+            NodeList nameList;
             String name;
 
-//            Parser timeParser = new Parser();
-//            timeParser.setInputHTML(address);
-//            HasAttributeFilter timeFilter = new HasAttributeFilter (attributeTime, attributeTimeValue);
-//            NodeList timeList = timeParser.parse(timeFilter);
-//            String time;
+            Parser timeParser = new Parser();
+            HasAttributeFilter timeFilter = new HasAttributeFilter ("kh:widget", "seance");
+            NodeList timeList;
+            String time;
 
-            for(int i = 0; i < nameList.size(); i++) {
+            System.out.println("MORI CINEMA nearest movie sessions for today:\n");
 
-                name = nameList.elementAt(i).toHtml();
-                name = name.substring(name.indexOf('>') + 1, name.length() - 4);
-                System.out.println(name);
+            for (int i = 0; i < blockList.size(); i++) {
+
+                block = blockList.elementAt(i).toHtml();
+                timeParser.setInputHTML(block);
+                nameParser.setInputHTML(block);
+                timeList = timeParser.parse(timeFilter);
+                nameList = nameParser.parse(nameFilter);
+
+                if (timeList.size() > 0) {
+                    time = timeList.elementAt(0).toHtml();
+                    time = time.replaceAll(" ", "");
+                    time = time.substring(time.indexOf('>') + 2, time.indexOf('>') + 7);
+                    System.out.print(time + "  ");
+                } else if (nameList.size() > 0) {
+                    System.out.print("--:--  ");
+                }
+
+                if (nameList.size() > 0) {
+                    name = nameList.elementAt(0).toHtml();
+                    name = name.substring(name.indexOf('>') + 1, name.length() - 4);
+                    System.out.println(name);
+                }
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "nothing";
-    }
-
-    public static void main (String args[]) {
-
-        new WantGoToCinema().run("https://mori-cinema.ru/cinema_detail/76_mori_sinema_krasnogorsk/schedule.php");
+        return address;
     }
 }
